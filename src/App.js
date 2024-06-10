2566ee22d81412eaecbdad458460d38d22b47507
kn7-pc <ga.rodrigo2005@gmail.com>
2024-06-10 13:34:40 -0300

    primeiro commit, colocando o projeto no git

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá meu app de react kkkkkkkkkkk</h1>
+      <p>Esse ta sendo meu primeiro codigdawdawo em react</p>
+    </div>
+  );
+}
+
+export default App;
