8af2ad3d41022f9b4e90dfcc877965b4687c1eef
kn7-pc <ga.rodrigo2005@gmail.com>
2024-06-10 23:05:52 -0300

    um pouco de css, add de imagem, primeira variavel/constante criada

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = "PokeQuiz In React"
+  const imageDog = "https://cdn.pixabay.com/photo/2024/02/26/19/39/monochrome-image-8598798_1280.jpg"
+
   return (
-    <div className="App">
-      <h1>Olá meu app de react kkkkkkkkkkk</h1>
-      <p>Esse ta sendo meu primeiro codigdawdawo em react</p>
+    <div id="container">
+      <div id="header">
+            <h1 id="logo">{title}</h1>
+            <h3 id="sublogo">My first contact with React, and for will have funny, i create a quiz of Pokemon and your types</h3>
+            <ul id="menu">
+                <li><a href= "http://localhost:3000"><strog>Home</strog></a></li>
+            </ul>
+      </div>
+      <div id="imagemDog">
+        <img src={imageDog} alt="imagem de um cachorro"/>
+      </div>
+      <div id="footer">
+        <p>&copy; Fiel a minha namorada BIA, jogador de fortnite e mandrake em tempos passados...</p>
+      </div>
     </div>
   );
 }
