1765d65916655aa7aec4f6ce713654bc7acde7a0
kn7-pc <ga.rodrigo2005@gmail.com>
2024-06-19 12:41:49 -0300

    PROPS

--- src/components/PokemonInfos.js
+++ src/components/PokemonInfos.js
@@ -1,10 +1,10 @@
-function PokemonInfos (props){
+function PokemonInfos ({type, type2, evolution, generation}){
 
     return(
         <div>
-            <h2>Types: {props.type} and {props.type2}</h2>
-            <h3>Evolution: {props.evolution}</h3>
-            <h3>Generation: {props.generation}</h3>
+            <h2>Types: {type} and {type2}</h2>
+            <h3>Evolution: {evolution}</h3>
+            <h3>Generation: {generation}</h3>
         </div>
     )
 
